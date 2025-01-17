f0c907debc5556812aac21b1eb62ad9fb8f464e6
saichaitanya518 <saichaitanya518@gmail.com>
2025-01-17 19:59:44 +0530

    first commit

---
+++ src/main/java/com/example/bookapigateway/BookApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookapigateway/BookApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
